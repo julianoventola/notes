58251a2f8e9ae5f759180e18258534091c6a12ae
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-17 19:41:46 -0300

    refactor: call home page and remove debug banner

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:notes/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Notes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        color: Colors.white,
-      ),
+      home: HomePage(),
     );
   }
 }
