2a368d236f0e782e7116f405e47f256f74f23fd8
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-17 20:01:52 -0300

    feat: home page using listview and card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notes = <String>[];
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('NOTES'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          notes.add('Item ${notes.length + 1}');
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(notes[index]),
+            ),
+          );
+        },
+      ),
     );
   }
 }
